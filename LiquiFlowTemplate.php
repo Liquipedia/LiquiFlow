a20e58e3eec7e952bbb6140b899dbdf1133d9188
FO-nTTaX <fo.nttax@yahoo.de>
2016-06-15 18:36:21 +0200

    alignment

--- LiquiFlowTemplate.php
+++ LiquiFlowTemplate.php
@@ -606,18 +606,18 @@ $footerLinks = $this->getFooterLinks();
 
 			<div class="col-md-2 col-sm-3 col-xs-12">
 				<div class="col">
-				<h4><?php $this->msg( 'liquiflow-our-wikis') ?></h4>
-				<ul>
-					<li><a href="http://wiki.teamliquid.net/starcraft/" target="_blank">Brood War</a></li>
-					<li><a href="http://wiki.teamliquid.net/counterstrike/" target="_blank">Counter-Strike</a></li>
-					<li><a href="http://wiki.teamliquid.net/dota2/" target="_blank">Dota 2</a></li>
-					<li><a href="http://wiki.teamliquid.net/hearthstone/" target="_blank">Hearthstone</a></li>
-					<li><a href="http://wiki.teamliquid.net/heroes/" target="_blank">Heroes of the Storm</a></li>
-					<li><a href="http://wiki.teamliquid.net/overwatch/" target="_blank">Overwatch</a></li>
-					<li><a href="http://wiki.teamliquid.net/smash/" target="_blank">Smash</a></li>
-					<li><a href="http://wiki.teamliquid.net/starcraft2/" target="_blank">StarCraft II</a></li>
-					<li><a href="http://wiki.teamliquid.net/commons/" target="_blank">Commons</a></li>
-				</ul>
+					<h4><?php $this->msg( 'liquiflow-our-wikis') ?></h4>
+					<ul>
+						<li><a href="http://wiki.teamliquid.net/starcraft/" target="_blank">Brood War</a></li>
+						<li><a href="http://wiki.teamliquid.net/counterstrike/" target="_blank">Counter-Strike</a></li>
+						<li><a href="http://wiki.teamliquid.net/dota2/" target="_blank">Dota 2</a></li>
+						<li><a href="http://wiki.teamliquid.net/hearthstone/" target="_blank">Hearthstone</a></li>
+						<li><a href="http://wiki.teamliquid.net/heroes/" target="_blank">Heroes of the Storm</a></li>
+						<li><a href="http://wiki.teamliquid.net/overwatch/" target="_blank">Overwatch</a></li>
+						<li><a href="http://wiki.teamliquid.net/smash/" target="_blank">Smash</a></li>
+						<li><a href="http://wiki.teamliquid.net/starcraft2/" target="_blank">StarCraft II</a></li>
+						<li><a href="http://wiki.teamliquid.net/commons/" target="_blank">Commons</a></li>
+					</ul>
 				</div>
 			</div>
 

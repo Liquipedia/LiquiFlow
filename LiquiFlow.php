ed9066ea82001d9426f51ec563ed0553e6e51f24
FO-nTTaX <fo.nttax@yahoo.de>
2016-06-28 15:35:56 +0200

    remove unnecessary spaces

--- LiquiFlow.php
+++ LiquiFlow.php
@@ -2,7 +2,7 @@
 if (function_exists('wfLoadSkin')) {
 	wfLoadSkin('LiquiFlow');
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['LiquiFlow'] = __DIR__ . '/i18n';               
+	$wgMessagesDirs['LiquiFlow'] = __DIR__ . '/i18n';
 	/* wfWarn(
 		'Deprecated PHP entry point used for LiquiFlow skin. Please use wfLoadSkin instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'

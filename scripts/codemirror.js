b6ad7f28146b976bfdd55eab492487f320af614d
FO-nTTaX <fo.nttax@yahoo.de>
2016-07-11 17:59:38 +0200

    fix copypaste in chrome

--- scripts/codemirror.js
+++ scripts/codemirror.js
@@ -1,3 +1,29 @@
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(searchString, position) {
+		var subjectString = this.toString();
+		if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+			position = subjectString.length;
+		}
+		position -= searchString.length;
+		var lastIndex = subjectString.indexOf(searchString, position);
+		return lastIndex !== -1 && lastIndex === position;
+	};
+}
+
+if (!String.prototype.includes) {
+	String.prototype.includes = function(search, start) {
+		'use strict';
+		if (typeof start !== 'number') {
+			start = 0;
+		}
+		if (start + search.length > this.length) {
+			return false;
+		} else {
+			return this.indexOf(search, start) !== -1;
+		}
+	};
+}
+
 $(document).ready(function() {
 	function invoke_codemirror(codemirror_id) {
 		var editmode;
@@ -19,7 +45,6 @@ $(document).ready(function() {
 			mwextModes: mw.config.get( 'liquiflowCodemirrorExtModes' ),
 			lineNumbers: true,
 			mode: editmode,
-			lineWrapping: true,
 			autofocus: true,
 			flattenSpans: false,
 			matchBrackets: true,

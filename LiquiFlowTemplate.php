7b8369191f3935cf9a068e1dc4b3db1edfd6d786
FO-nTTaX <fo.nttax@yahoo.de>
2017-03-17 16:09:17 +0100

    remove empty statement

--- LiquiFlowTemplate.php
+++ LiquiFlowTemplate.php
@@ -704,9 +704,9 @@ $footerLinks = $this->getFooterLinks();
 			<ul id="f-list">
 				<?php
 				if (isset($footerLinks['info']) && is_array($footerLinks['info'])) {
-					foreach ( $footerLinks['info'] as $link ) {	?>
+					foreach ( $footerLinks['info'] as $link ) { ?>
 						<li id="footer-info-<?php echo $link; ?>">
-							<?php echo $this->html( $link );;?>
+							<?php echo $this->html( $link ); ?>
 						</li>
 				<?php
 					}

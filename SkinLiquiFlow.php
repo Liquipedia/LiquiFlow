378b7cd377d9f9d839ca6081885907e7eb22eaf2
FO-nTTaX <fo.nttax@yahoo.de>
2017-05-18 17:06:25 +0200

    added some header tags

--- SkinLiquiFlow.php
+++ SkinLiquiFlow.php
@@ -18,7 +18,17 @@ class SkinLiquiFlow extends SkinTemplate {
 	public function initPage( OutputPage $out ) {
 
 		parent::initPage( $out );
-		global $wgStylePath;
+		global $wgStylePath, $wgServer, $wgSitename;
+		$faviconPath = $wgStylePath . '/LiquiFlow/images/favicon/';
+		$title = $this->getTitle();
+		$description = trim( htmlspecialchars( substr( str_replace( array( "\n", "\r", "\n", "[edit]", "  ", "  ", "  " ), ' ', strip_tags( $out->getHTML() ) ), 0, 150 ) ) ) . '...';
+		$addAutoMeta = true;
+		foreach( $out->getMetaTags() as $metaTag) {
+			if( $metaTag[0] == 'description' ) {
+				$addAutoMeta = false;
+				$description = htmlspecialchars( $metaTag[1] );
+			}
+		}
 
 		// add text to recruit people from landing page
 		$out->addHeadItem('recruitment',
@@ -33,25 +43,65 @@ class SkinLiquiFlow extends SkinTemplate {
 			"\tHi you, yes you who's looking at our source code! Are you a website specialist?\n" .
 			"\tWe are looking for people to help us with our templates, especially with mobile development.\n" .
 			"\tIf you want to help, be sure to visit us on our IRC channel #liquipedia on QuakeNet,\n" .
-			"\tor send us an email to liquipedia <at> teamliquid <dot> net!\n" .
+			"\tjoin us on discord (http://liquipedia.net/discord), or send us an email to\n" .
+			"liquipedia <at> teamliquid <dot> net!\n" .
 			"-->");
 
 		// Append CSS which includes IE only behavior fixes for hover support -
 		// this is better than including this in a CSS file since it doesn't
 		// wait for the CSS file to load before fetching the HTC file.
 		// $min = $this->getRequest()->getFuzzyBool( 'debug' ) ? '' : '.min';
-
-		$out->addHeadItem( 'ie-edge', "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+		$out->addHeadItem( 'ie-edge', '<meta http-equiv="X-UA-Compatible" content="IE=edge">');
 		$out->addHeadItem( 'x-ie8-fix',
 			"<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->\n" .
-		"<!--[if lt IE 9]>\n" .
-		"<script src=\"https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js\"></script>\n" .
-		"<script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>\n" .
-		"<![endif]-->");
-		$out->addHeadItem( 'responsive', "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-		$out->addHeadItem( 'mobile-head-color', "<meta name=\"theme-color\" content=\"#052b4c\">" );
-		//$out->addHeadItem( 'icon', "<link rel=\"icon\" href=\"" . $wgStylePath . "/LiquiFlow/images/favicon/favicon.ico\">" );
-		//$out->addHeadItem( 'icon-192x192', "<link rel=\"icon\" sizes=\"192x192\" href=\"" . $wgStylePath . "/LiquiFlow/images/favicon/liquipedia_logo_192x192.png\">" );
+			"<!--[if lt IE 9]>\n" .
+			"<script src=\"https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js\"></script>\n" .
+			"<script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>\n" .
+			"<![endif]-->");
+
+		// Meta tags for mobile
+		$out->addHeadItem( 'responsive', '<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+		$out->addHeadItem( 'mobile-head-color', '<meta name="theme-color" content="#052b4c">' );
+
+		// Favicons
+		$out->addHeadItem( 'favicons', 
+			'<link rel="apple-touch-icon-precomposed" sizes="57x57" href="' . $faviconPath . 'apple-touch-icon-57x57.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="' . $faviconPath . 'apple-touch-icon-114x114.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="' . $faviconPath . 'apple-touch-icon-72x72.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . $faviconPath . 'apple-touch-icon-144x144.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="60x60" href="' . $faviconPath . 'apple-touch-icon-60x60.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="120x120" href="' . $faviconPath . 'apple-touch-icon-120x120.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="76x76" href="' . $faviconPath . 'apple-touch-icon-76x76.png" />'
+			. '<link rel="apple-touch-icon-precomposed" sizes="152x152" href="' . $faviconPath . 'apple-touch-icon-152x152.png" />'
+			. '<link rel="icon" type="image/png" href="' . $faviconPath . 'favicon-196x196.png" sizes="196x196" />'
+			. '<link rel="icon" type="image/png" href="' . $faviconPath . 'favicon-96x96.png" sizes="96x96" />'
+			. '<link rel="icon" type="image/png" href="' . $faviconPath . 'favicon-32x32.png" sizes="32x32" />'
+			. '<link rel="icon" type="image/png" href="' . $faviconPath . 'favicon-16x16.png" sizes="16x16" />'
+			. '<link rel="icon" type="image/png" href="' . $faviconPath . 'favicon-128x128.png" sizes="128x128" />'
+			. '<meta name="application-name" content="' . $wgSitename . '"/>'
+			. '<meta name="msapplication-TileColor" content="#ffffff" />'
+			. '<meta name="msapplication-TileImage" content="' . $faviconPath . 'mstile-144x144.png" />'
+			. '<meta name="msapplication-square70x70logo" content="' . $faviconPath . 'mstile-70x70.png" />'
+			. '<meta name="msapplication-square150x150logo" content="' . $faviconPath . 'mstile-150x150.png" />'
+			. '<meta name="msapplication-wide310x150logo" content="' . $faviconPath . 'mstile-310x150.png" />'
+			. '<meta name="msapplication-square310x310logo" content="' . $faviconPath . 'mstile-310x310.png" />' );
+
+		// Do stuff for SEO
+		$out->addHeadItem( 'ogproperties', 
+			'<meta property="og:type" content="article">'
+			. '<meta property="og:image" content="' . $wgServer . $faviconPath . 'mstile-310x310.png" />'
+			. '<meta property="og:url" content="' . $title->getFullURL() . '" />'
+			. '<meta property="og:title" content="' . htmlspecialchars( $out->getPageTitle() ) . '" />'
+			. '<meta property="og:description" content="' . $description . '" />' );
+		$addAutoMeta = true;
+		foreach( $out->getMetaTags() as $metaTag) {
+			if( $metaTag[0] == 'description' ) {
+				$addAutoMeta = false;
+			}
+		}
+		if( $addAutoMeta ) {
+			$out->addMeta( 'description', $description );
+		}
 
 		Hooks::run( 'LiquiFlowAdStartCode', array( &$out ) );
 

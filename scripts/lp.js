a381914e64e16dad3f7f04753b061c9dfbb43954
FO-nTTaX <fo.nttax@yahoo.de>
2017-06-26 13:35:28 +0200

    disable submit button after clicking to avoid self-edit conflicts

--- scripts/lp.js
+++ scripts/lp.js
@@ -129,6 +129,7 @@ $(document).ready(function () {
 		$('[data-toggle="tooltip"]').tooltip()
 	});
 
+	/* Disable save buttoin after submit to avoid self-editconflicts */
 	$('#wpSave').click(function() {
 		$(this).attr('disabled', 'disabled');
 		$('#editform').submit();

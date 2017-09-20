8c0f12b1afe14cfee8e0aababa0a4778f5251486
FO-nTTaX <fo.nttax@yahoo.de>
2017-09-20 13:24:13 +0200

    js cleanup

--- scripts/ie10-viewport-bug-workaround.js
+++ scripts/ie10-viewport-bug-workaround.js
@@ -8,15 +8,13 @@
 // See the Getting Started docs for more information:
 // http://getbootstrap.com/getting-started/#support-ie10-width
 
-( function () {
+( function() {
 	'use strict';
 	if(navigator.userAgent.match(/IEMobile\/10\.0/)) {
-		var msViewportStyle = document.createElement('style');
+		var msViewportStyle = document.createElement( 'style' );
 		msViewportStyle.appendChild(
-			document.createTextNode(
-				'@-ms-viewport{width:auto!important}'
-			)
+			document.createTextNode( '@-ms-viewport{width:auto!important}' )
 		);
-		document.querySelector('head').appendChild(msViewportStyle);
+		document.querySelector( 'head' ).appendChild( msViewportStyle );
 	}
 } )();
--- scripts/lp.js
+++ scripts/lp.js
@@ -1,14 +1,3 @@
-( function() {
-	'use strict';
-	if(navigator.userAgent.match(/IEMobile\/10\.0/)) {
-		var msViewportStyle = document.createElement( 'style' );
-		msViewportStyle.appendChild(
-			document.createTextNode( '@-ms-viewport{width:auto!important}' )
-		);
-		document.querySelector( 'head' ).appendChild( msViewportStyle );
-	}
-} )();
-
 $( document ).ready(function() {
 	
 	/* a click on mobile search closes a possibly opened menu/toc */

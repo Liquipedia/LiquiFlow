03e14d248a4ea51b44fa9e09def2aa3bcbd0afe9
FO-nTTaX <fo.nttax@yahoo.de>
2017-11-27 13:13:22 +0100

    define font for editor so it looks better especially for asian editors

--- SkinLiquiFlow.php
+++ SkinLiquiFlow.php
@@ -208,7 +208,7 @@ class SkinLiquiFlow extends SkinTemplate {
 		$user = $this->getSkin()->getUser();
 		parent::setupSkinUserCss( $out );
 
-		$out->addStyle( 'https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,700,700italic%7CDroid+Sans+Mono%7CRoboto:500' );
+		$out->addStyle( 'https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,700,700italic%7CDroid+Sans+Mono%7CRoboto:500%7CSource+Code+Pro:400,700' );
 		$styles = array( 'mediawiki.skinning.interface', 'skins.liquiflow', 'skins.liquiflow.bottom' );
 		$out->addModuleStyles( $styles );
 		if( $out->getResourceLoader()->isModuleRegistered( 'skins.liquiflow.theme.' . substr($wgScriptPath, 1) ) ) {

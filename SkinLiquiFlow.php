e780fb74cab016c27b7a3b28c3452f9d5df43130
FO-nTTaX <fo.nttax@yahoo.de>
2018-01-04 17:28:52 +0100

    compat update (deprecated IE8/9)

--- SkinLiquiFlow.php
+++ SkinLiquiFlow.php
@@ -140,20 +140,12 @@ class SkinLiquiFlow extends SkinTemplate {
 			. "\tan email to liquipedia <at> teamliquid <dot> net!\n"
 			. "-->");
 
-		// Append CSS which includes IE only behavior fixes for hover support -
-		// this is better than including this in a CSS file since it doesn't
-		// wait for the CSS file to load before fetching the HTC file.
-		$out->addHeadItem( 'ie-edge', '<meta http-equiv="X-UA-Compatible" content="IE=edge">');
-		// HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
-		$out->addHeadItem( 'x-ie8-fix',
-			"<!--[if lt IE 9]>\n"
-			. "<script src=\"https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js\"></script>\n"
-			. "<script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>\n"
-			. "<![endif]-->");
+		// Edge compatibility mode (don't run in outdated compat)
+		$out->addHeadItem( 'ie-edge', '<meta http-equiv="X-UA-Compatible" content="IE=edge">' );
 
 		// Meta tags for mobile
 		$out->addHeadItem( 'responsive', '<meta name="viewport" content="width=device-width, initial-scale=1.0">');
-		$out->addHeadItem( 'theme-color', '<meta name="theme-color" content="' . LiquiFlowColors::getSkinColors( substr($wgScriptPath, 1), 'wiki-dark') . '">' );
+		$out->addHeadItem( 'theme-color', '<meta name="theme-color" content="' . LiquiFlowColors::getSkinColors( substr( $wgScriptPath, 1 ), 'wiki-dark' ) . '">' );
 
 		// Favicons
 		$out->addHeadItem( 'favicons', 

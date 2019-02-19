2375def1c398e0f200b66ac590b9662ba95e2fdd
FO-nTTaX <fo.nttax@yahoo.de>
2019-02-19 16:11:58 +0100

    color things

--- LiquiFlowColors.php
+++ LiquiFlowColors.php
@@ -18,12 +18,12 @@ class Colors {
 		'artifact' => array(
 			'wiki-dark' => '#484848',
 			'wiki-light' => '#fbf6df',
-			'body-bg' => '#dee2ec',
+			'body-bg' => '#e5e5e5',
 		),
 		'battlerite' => array(
 			'wiki-dark' => '#955623',
 			'wiki-light' => '#fbecdf',
-			'body-bg' => '#dee2ec',
+			'body-bg' => '#e5e5e5',
 		),
 		'callofduty' => array(
 			'wiki-dark' => '#4a4a4a',
@@ -33,7 +33,7 @@ class Colors {
 		'clashroyale' => array(
 			'wiki-dark' => '#31519c',
 			'wiki-light' => '#e6e6e6',
-			'body-bg' => '#e6e6e6',
+			'body-bg' => '#e5e5e5',
 		),
 		'commons' => array(
 			'wiki-dark' => '#e5e5e5',
@@ -42,13 +42,15 @@ class Colors {
 		),
 		'counterstrike' => array(
 			'wiki-dark' => '#7b932d',
+			'wiki-dark-target' => '#54711b',
 			'wiki-light' => '#cde5b6',
+			'wiki-light-target' => '#f0f9de',
 			'body-bg' => '#e5e5e5',
 		),
 		'crossfire' => array(
 			'wiki-dark' => '#0e4659',
 			'wiki-light' => '#b9953f',
-			'body-bg' => '#dcd7e6',
+			'body-bg' => '#e5e5e5',
 		),
 		'dota2' => array(
 			'wiki-dark' => '#93352d',
@@ -98,17 +100,17 @@ class Colors {
 		'pubg' => array(
 			'wiki-dark' => '#6a4a3c',
 			'wiki-light' => '#edc951',
-			'body-bg' => '#f8fafb',
+			'body-bg' => '#e5e5e5',
 		),
 		'quake' => array(
 			'wiki-dark' => '#484038',
 			'wiki-light' => '#d1753e',
-			'body-bg' => '#eeeeee',
+			'body-bg' => '#e5e5e5',
 		),
 		'rainbowsix' => array(
 			'wiki-dark' => '#865879',
 			'wiki-light' => '#f0e0a4',
-			'body-bg' => '#f8f2ed',
+			'body-bg' => '#e5e5e5',
 		),
 		'rocketleague' => array(
 			'wiki-dark' => '#003f84',
@@ -138,30 +140,72 @@ class Colors {
 		'trackmania' => array(
 			'wiki-dark' => '#337238',
 			'wiki-light' => '#c3bac5',
-			'body-bg' => '#e2dce0',
+			'body-bg' => '#e5e5e5',
 		),
 		'warcraft' => array(
 			'wiki-dark' => '#aab9b7',
 			'wiki-light' => '#1e4555',
-			'body-bg' => '#f5f1fa',
+			'body-bg' => '#e5e5e5',
 		),
 		'worldofwarcraft' => array(
 			'wiki-dark' => '#831174',
 			'wiki-light' => '#d1e2f1',
-			'body-bg' => '#f1e8ea',
+			'body-bg' => '#e5e5e5',
 		),
 	);
 
 	public static function getSkinColors( $wiki, $entity = null ) {
-		if ( $entity == null && isset( self::$colors[ $wiki ] ) ) {
-			return self::$colors[ $wiki ];
-		} elseif ( $entity == null ) {
-			return self::$colors[ 'commons' ];
-		} elseif ( isset( self::$colors[ $wiki ] ) ) {
-			return self::$colors[ $wiki ][ $entity ];
+		$colors = self::getSkinColorsInternal( $wiki );
+		if ( is_null( $entity ) ) {
+			return $colors;
 		} else {
-			return self::$colors[ 'commons' ][ $entity ];
+			return $colors[ $entity ];
 		}
+	}
+
+	private static function getSkinColorsInternal( $wiki ) {
+		$colors = null;
+		if ( isset( self::$colors[ $wiki ] ) ) {
+			$colors = self::$colors[ $wiki ];
+		} else {
+			$colors = self::$colors[ 'commons' ];
+		}
+		if ( array_key_exists( 'wiki-dark-target', $colors ) ) {
+			$colors[ 'wiki-dark' ] = self::getGradient( $colors[ 'wiki-dark-target' ], $colors[ 'wiki-dark' ] );
+			unset( $colors[ 'wiki-dark-target' ] );
+		}
+		if ( array_key_exists( 'wiki-light-target', $colors ) ) {
+			$colors[ 'wiki-light' ] = self::getGradient( $colors[ 'wiki-light-target' ], $colors[ 'wiki-light' ] );
+			unset( $colors[ 'wiki-light-target' ] );
+		}
+		return $colors;
+	}
+
+	private static function getGradient( $hex1, $hex2 ) {
+		$per = self::getPer();
+		$new = [];
+		$parts1 = str_split( substr( $hex1, 1 ), 2 );
+		$parts2 = str_split( substr( $hex2, 1 ), 2 );
+		for ( $i = 0; $i < 3; $i++ ) {
+			$h1 = hexdec( $parts1[ $i ] );
+			$h2 = hexdec( $parts2[ $i ] );
+			$new[ $i ] = str_pad( dechex( round( $h1 + ( $h2 - $h1 ) * ( 1 - $per ) ) ), 2, '0' );
+		}
+		$ret = '#' . implode( '', $new );
+		return $ret;
+	}
+
+	private static function getPer() {
+		$d = ( new \DateTime( ) )->format( 'U' );
+		$d1 = ( new \DateTime( '2019-02-19' ) )->format( 'U' );
+		$d2 = ( new \DateTime( '2019-08-19' ) )->format( 'U' );
+		if ( $d > $d2 ) {
+			return 1;
+		}
+		$i1 = $d - $d1;
+		$i2 = $d2 - $d1;
+		$per = $i1 / $i2;
+		return $per;
 	}
 
 }
